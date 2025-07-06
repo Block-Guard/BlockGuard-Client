06d2f7a8fd1f444a21d31b40a3212925caea6445
glyph8 <newdy0808@gmail.com>
2025-07-06 16:33:46 +0900

    feat(LinkNumberCheck): 입력 onchange적용

--- src/pages/HomePage/LinkNumberCheck.tsx
+++ src/pages/HomePage/LinkNumberCheck.tsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
+
 const LinkNumberCheck = () => {
+
+    const [urlNumber, setUrlNumber] = useState('');
+
+    const activeEnter = (e) => {
+        if (e.key === "Enter") {
+            alert(urlNumber); //임시
+        }
+    }
+
     return (
         <div
-            className="w-[344px] h-28 p-4
+            className="flex flex-col justify-between w-[344px] h-28 pr-4 pl-4 pb-4 pt-3
                  bg-white/10 rounded-[20px] border-2 border-white/60"
         >
-            <div className="justify-center text-white text-2xl font-bold font-pretendard leading-9">
+            <div className="justify-center text-white text-2xl font-bold leading-9">
                 클릭 한 번으로 위험 확인
             </div>
-            <img src="icons/Search-icon.svg" alt="돋보기 아이콘" />
-            <input type="text" placeholder="URL / 전화번호를 검색해보세요" />
-            <div className="w-80 h-0.5 bg-white rounded-[90px]" />
+            <div className="flex flex-row text-white/50 text-sm font-medium leading-none">
+                <img src="icons/Search-icon.svg" className="mr-1" alt="돋보기 아이콘" />
+                <input type="text" 
+                    className="w-full focus:outline-none focus:ring-0 text-white text-sm font-medium leading-none" 
+                    onChange={(e) => setUrlNumber(e.target.value)}
+                    onKeyDown={(e) => activeEnter(e)}
+                    placeholder="URL / 전화번호를 검색해보세요" />
+            </div>
+            <div className="w-78 h-0.5 bg-white rounded-[90px]" />
         </div>
     )
 }
