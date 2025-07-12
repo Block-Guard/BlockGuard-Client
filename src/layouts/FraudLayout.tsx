6b477ebdf337d152edcb51b03c8f4caabb0a5cc2
holdn2 <yoocy01@gmail.com>
2025-07-13 01:01:34 +0900

    refactor: 이미지 파일 src/assets 폴더로 이동 후 각 파일에 import로 이용하도록 수정 완료

--- src/layouts/FraudLayout.tsx
+++ src/layouts/FraudLayout.tsx
@@ -5,58 +5,36 @@ import LeftArrowWhiteIcon from "../assets/icons/ArrowLeftWhite-icon.svg";
 import LeftArrowIcon from "../assets/icons/ArrowLeftBlack-icon.svg";
 
 const FraudLayout = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const {
+    selectedAnswer,
+    progress,
+    setProgress,
+    recordAnswerAndProceed,
+    reset,
+  } = useFraudStore();
+  const heightSize =
+    location.pathname === "/fraud-analysis"
+      ? "h-[calc(100vh-48px)]"
+      : "h-[calc(100vh-140px)]";
 
-    const location = useLocation();
-    const navigate = useNavigate();
-    const { selectedAnswer, progress, setProgress, recordAnswerAndProceed, reset } = useFraudStore();
-    const heightSize = location.pathname === "/fraud-analysis" ? "h-[calc(100vh-48px)]" : "h-[calc(100vh-140px)]"
-
-    const handleBackClick = () => {
-        console.log("현재 백클릭에서의 progress : ", progress)
-        if (progress <= 1) {
-            reset();
-            navigate(-1);
-            return;
-        }
-        if (progress >= 7) {
-            navigate(-1);
-        }
-        setProgress(progress - 1);
+  const handleBackClick = () => {
+    console.log("현재 백클릭에서의 progress : ", progress);
+    if (progress <= 1) {
+      reset();
+      navigate(-1);
+      return;
+    }
+    if (progress >= 7) {
+      navigate(-1);
     }
     setProgress(progress - 1);
   };
 
-
-    const handleBtnClick = () => {
-        recordAnswerAndProceed(navigate);
-    }
-
-    return (
-        <div className="flex flex-col justify-between w-full h-full">
-            {location.pathname === "/fraud-analysis" ? (
-                <div className="w-full h-12 bg-primary-400 outline-none">
-                    <div className="flex justify-start pl-6 pt-3.5 pb-3.5">
-                        <button onClick={() => navigate(-1)}>
-                            <img src="/icons/ArrowLeftWhite-icon.svg" alt="뒤로가기" />
-                        </button>
-                    </div >
-                </div>
-            ) : (
-                <div className="w-full h-12">
-                    <div className="flex justify-start pl-6 pt-3.5 pb-3.5">
-                        <button onClick={handleBackClick}>
-                            <img src="/icons/ArrowLeftBlack-icon.svg" alt="뒤로가기" />
-                        </button>
-                    </div >
-
-                    <div className="w-full h-1.5 relative">
-                        <div className="w-full h-[5px] left-0 top-0 absolute bg-gray-200" />
-                        <div className="h-[5px] left-0 top-0 absolute bg-blue-500 rounded-tr-[90px] rounded-br-[90px]"
-                            style={{ width: `${progress * 10}%` }} />
-                    </div>
-                </div >
-            )}
-
+  const handleBtnClick = () => {
+    recordAnswerAndProceed(navigate);
+  };
 
   return (
     <div className="flex flex-col justify-between w-full h-full">
@@ -92,15 +70,19 @@ const FraudLayout = () => {
         <Outlet /> {/* 사기분석 설문 내용 렌더링 */}
       </main>
 
-            {location.pathname === "/fraud-analysis" ? null : (
-                <div className="ml-6 mr-6 mb-8">
-                    <Button onClick={handleBtnClick} size="lg" isHighlight={false} disabled={selectedAnswer === null && progress < 7}>
-                        다음
-                    </Button>
-                </div>
-            )}
-        </div >
-    );
-}
+      {location.pathname === "/fraud-analysis" ? null : (
+        <div className="ml-6 mr-6 mb-8">
+          <Button
+            onClick={handleBtnClick}
+            size="lg"
+            isHighlight={false}
+            disabled={selectedAnswer === null && progress < 7}
+          >
+            다음
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
 export default FraudLayout;
-
--- src/pages/FraudSurvey/FraudLandingPage/FraudLandingPage.tsx
+++ src/pages/FraudSurvey/FraudLandingPage/FraudLandingPage.tsx
@@ -1,46 +1,45 @@
 import { useNavigate } from "react-router-dom";
+import HomeBgShieild from "../../../assets/homeBackgroundShield.svg";
+import Blockee from "../../../assets/Blockee-cropped-fit.svg";
 
 const FraudLandingPage = () => {
-    const navigate = useNavigate();
-    const handleClick = () => {
-        navigate("/fraud-analysis/survey")
-    }
-    return (
-        <div className="w-full h-full relative p-6 bg-primary-400"
-            onClick={handleClick}>
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/fraud-analysis/survey");
+  };
+  return (
+    <div
+      className="w-full h-full relative p-6 bg-primary-400"
+      onClick={handleClick}
+    >
+      <div className="text-white text-3xl font-extrabold leading-loose">
+        AI 사기유형 진단 서비스
+      </div>
 
-            <div className="text-white text-3xl font-extrabold leading-loose">
-                AI 사기유형 진단 서비스
-            </div>
+      <div className="text-gray-200 text-lg font-normal leading-relaxed">
+        의심스러운 연락을 받았다면 지금 피싱여부를 진단해보세요!
+      </div>
 
-            <div className="text-gray-200 text-lg font-normal leading-relaxed">
-                의심스러운 연락을 받았다면
-                지금 피싱여부를 진단해보세요!
-            </div>
+      <img src={HomeBgShieild} className="absolute right-0 top-33" />
 
-            <img
-                src="/homeBackgroundShield.svg"
-                className="absolute right-0 top-33"
-            />
+      <div className="w-full mt-35 mb-5 text-white text-xl font-medium leading-normal">
+        ‘AI 사기유형 진단 서비스’는 피싱 상황이 의심될 때 대화 내용 분석을 통해
+        위험 상황을 판단해 주는 서비스예요.
+      </div>
+      <div className="w-full text-white text-xl font-medium leading-normal">
+        분석을 위해 진행되는 사전 설문을 꼼꼼히 입력해 주실수록 진단의 정확도가
+        올라가요.
+      </div>
 
-            <div className="w-full mt-35 mb-5 text-white text-xl font-medium leading-normal">
-                ‘AI 사기유형 진단 서비스’는 피싱 상황이 의심될 때
-                대화 내용 분석을 통해 위험 상황을 판단해 주는 서비스예요.
-            </div>
-            <div className="w-full text-white text-xl font-medium leading-normal">
-                분석을 위해 진행되는 사전 설문을 꼼꼼히 입력해 주실수록
-                진단의 정확도가 올라가요.
-            </div>
+      <div className="absolute bottom-40 right-10">
+        <img src={Blockee} width="144px" height="126px" />
+      </div>
 
-            <div className="absolute bottom-40 right-10">
-                <img src="/Blockee-cropped-fit.svg" width="144px" height="126px" />
-            </div>
-
-            <div className="flex justify-center relative top-70 text-white/50 text-xl font-semibold leading-loose">
-                화면을 터치하여 시작하세요!
-            </div>
-        </div >
-    )
-}
+      <div className="flex justify-center relative top-70 text-white/50 text-xl font-semibold leading-loose">
+        화면을 터치하여 시작하세요!
+      </div>
+    </div>
+  );
+};
 
 export default FraudLandingPage;
