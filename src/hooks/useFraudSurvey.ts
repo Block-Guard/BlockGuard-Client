6bbeda29b4f60939480d200a098e7dfb4d816312
glyph8 <newdy0808@gmail.com>
2025-07-19 19:03:59 +0900

    fix: textarea resize 제거, 스크롤바 제거, 설문 이전단계 잘못된 이동 문제 해결

--- src/hooks/useFraudSurvey.ts
+++ src/hooks/useFraudSurvey.ts
@@ -65,31 +65,20 @@ export const useFraudSurvey = () => {
         const newProgress = progress - 1;
         setProgress(newProgress);
 
-        if (progress <= 0) {
+        if (newProgress <= 0) {
             // TODO: 초기화 및 뒤로 가기 로직
             navigate('/fraud-analysis/landing');
             return;
         }
-        if (progress === 6) {
-            navigate('/fraud-analysis/1-6');
+        if (newProgress === 6) {
+            navigate('/fraud-analysis/survey/1-6');
             return;
         }
-        if(progress > 6){
+        if(newProgress > 6){
             navigate(`/fraud-analysis/survey/${newProgress}`);
         }
 
     }, [progress, navigate]);
-
-    // const goToPrevStep = useCallback(() => {
-        
-    //     if (progress <= 1) {
-    //         // TODO: 초기화 및 뒤로 가기 로직
-    //         navigate('/fraud-analysis/landing');
-    //         return;
-    //     }
-    //     setProgress((p) => p - 1);
-    //     if (progress >= 7) navigate(-1);
-    // }, [progress, navigate]);
 
     // 훅이 반환하는 값들: 상태와 핸들러 함수
     return {
