7630f7583d0de84475b811b47c7c6d44d5804c5d
glyph8 <newdy0808@gmail.com>
2025-07-22 20:56:58 +0900

    feat: 위험도 표, 지시핀 이미지 균일화 및 배치

--- src/pages/FraudSurvey/AnalysisResultPage/AnalysisResultPage.tsx
+++ src/pages/FraudSurvey/AnalysisResultPage/AnalysisResultPage.tsx
@@ -3,58 +3,92 @@ import Header from "../../../components/Header/Header";
 import LeftArrowIcon from "../../../assets/icons/arrow-left-white-icon.svg";
 import WhiteX from "../../../assets/icons/x-white-icon.svg"
 import RiskChatBubble from "../../../assets/analysis-result/chat-bubble-result-risk.svg";
+import WarnChatBubble from "../../../assets/analysis-result/chat-bubble-result-warn.svg";
+import SafeChatBubble from "../../../assets/analysis-result/chat-bubble-result-safe.svg";
 import Blockee from "../../../assets/character-cropped-fit-image.svg";
 import Warning from "../../../assets/analysis-result/warning.svg";
 import PhoneCall from "../../../assets/icons/phone-call-icon.png";
 import Siren from "../../../assets/icons/siren-icon.svg";
 import SmartPhone from "../../../assets/analysis-result/smartphone-icon.png";
 import RiskRed from "../../../assets/analysis-result/risk-level-red.svg";
-import RiskYellow from "../../../assets/analysis-result/risk-level-red.svg";
-import RiskGreen from "../../../assets/analysis-result/risk-level-red.svg";
+import RiskYellow from "../../../assets/analysis-result/risk-level-yellow.svg";
+import RiskGreen from "../../../assets/analysis-result/risk-level-green.svg";
+import IndicatorArrow from "../../../assets/analysis-result/indicator-arrow.svg"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReactingCard from "./components/ReactingCard";
 
+const riskState = [
+    {
+        bgColor: "#F24E4E",
+        text: "사기 위험도가 높은 상황",
+        bubbleChat: RiskChatBubble,
+        boardImg: RiskRed,
+        degree: 135,
+    },
+    {
+        bgColor: "#FFCD42",
+        text: "사기 위험도 주의 상황",
+        bubbleChat: WarnChatBubble,
+        boardImg: RiskYellow,
+        degree: 90,
+    },
+    {
+        bgColor: "#40D479",
+        text: "사기 위험도가 낮은 상황",
+        bubbleChat: SafeChatBubble,
+        boardImg: RiskGreen,
+        degree: 45,
+    },
+];
 
+const dummyResponse = {
+    "code": 2014,
+    "message": "사기 분석이 완료되었습니다.",
+    "data": {
+        "riskLevel": "위험", // "주의", "안전"
+        "score": 94.2,
+        "estimatedFraudType": "기관 사칭형",//"PROSECUTOR_IMPERSONATION",
+        "keywords": ["검찰", "송금", "서민금융"],
+        "explanation": "검찰을 사칭해 송금 및 개인정보를 요구하며, 서민금융을 사유로 앱 설치를 유도하는 전형적인 사기입니다.",
+        "recommendedAction": "즉시 앱 삭제 및 112에 신고하세요."
+    }
+}
 
 const AnalysisResultPage = () => {
     const navigate = useNavigate();
-    const [bgColor, setBgColor] = useState("#F24E4E");
+
+    const [resultTheme, setResultTheme] = useState(riskState[1]);
+
     const handleBackClick = () => {
         navigate(-1);
         // navigate("/fraud-analysis/survey/9");
     };
-    const handleCloseClick = () => {
+    const handleGuardianClick = () => {
+        console.log("보호자 바텀시트");
+    }
+    const handleCallClick = () => {
+        console.log("전화 태그 연결");
+    }
+    const handleReportClick = () => {
+        navigate("/emergency/report-overview");
+    }
+    const handleHomeClick = () => {
         navigate("/home");
-    };
-
-    const dummyResponse = {
-        "code": 2014,
-        "message": "사기 분석이 완료되었습니다.",
-        "data": {
-            "riskLevel": "위험", // "주의", "안전"
-            "score": 94.2,
-            "estimatedFraudType": "기관 사칭형",//"PROSECUTOR_IMPERSONATION",
-            "keywords": ["검찰", "송금", "서민금융"],
-            "explanation": "검찰을 사칭해 송금 및 개인정보를 요구하며, 서민금융을 사유로 앱 설치를 유도하는 전형적인 사기입니다.",
-            "recommendedAction": "즉시 앱 삭제 및 112에 신고하세요."
-        }
     }
 
-    const setStaticInfo = () => {
-        setBgColor("white")
-        if (dummyResponse.data.riskLevel === "위험") {
-            setBgColor("#F24E4E")
+    useEffect(() => {
+        let flag = 0;
+        if (dummyResponse.data.riskLevel === "위험")
+            flag = 0;
+        if (dummyResponse.data.riskLevel === "주의")
+            flag = 1;
+        if (dummyResponse.data.riskLevel === "안전")
+            flag = 2;
 
-        }
-        if (dummyResponse.data.riskLevel === "주의") {
-            setBgColor("#FFCD42")
-
-        }
-        if (dummyResponse.data.riskLevel === "안전") {
-            setBgColor("#40D479")
-        }
-    }
+        flag = 2
+        setResultTheme(riskState[flag]);
+    }, [])
 
 
     return (
@@ -65,23 +99,23 @@ const AnalysisResultPage = () => {
                         <img src={LeftArrowIcon} alt="뒤로가기" />
                     </button>
                 }
-                bgColor={bgColor}
+                bgColor={resultTheme.bgColor}
                 title={
                     <div className="text-white text-xl font-bold leading-loose">
                         AI 분석 결과
                     </div>
                 }
                 rightChild={
-                    <button onClick={handleCloseClick}>
+                    <button onClick={handleHomeClick}>
                         <img src={WhiteX} alt="창닫기" className="w-6 h-6" />
                     </button>
                 }
             />
 
-            <div className="mt-[57px] h-37" style={{ backgroundColor: `${bgColor}` }}>
-                <div>
-                    <img src={RiskChatBubble} alt="위험 경고문구" className="w-51 h-18" />
-                    <img src={Blockee} alt="캐릭터" className="w-28 h-24"/>
+            <div className="mt-[57px] h-37" style={{ backgroundColor: `${resultTheme.bgColor}` }}>
+                <div className="p-0">
+                    <img src={resultTheme.bubbleChat} alt="위험 경고문구" className="w-51 h-18" />
+                    <img src={Blockee} alt="캐릭터" className="w-28 h-24 z-0" />
                     <img src={Warning} alt="위험 아이콘" className="w-12 h-10" />
                 </div>
             </div>
@@ -89,16 +123,26 @@ const AnalysisResultPage = () => {
             {/* 여기까지 상단 안내 */}
 
             <div className="flex flex-col items-center py-7.5 px-6">
-                <div className="text-red-500 text-3xl font-extrabold leading-10">
-                    사기 위험도가 높은 상황
+                <div className="text-3xl font-extrabold leading-10" style={{color:resultTheme.bgColor}}>
+                    {resultTheme.text}
                 </div>
-                <div>
-                    <img src={RiskRed} alt="위험도 표" />
+
+                <div className="h-42">
+                    <img src={resultTheme.boardImg} alt="위험도 표"
+                        className="z-0" />
+                    <div className="relative left-27 bottom-11 w-16 h-16 flex items-center justify-end">
+                        <img src={IndicatorArrow} alt="위험지시핀"
+                            className="absolute"
+                            style={{
+                                transform: `rotate(${resultTheme.degree}deg)`,
+                                transformOrigin: "95% center",
+                            }} />
+                    </div>
                 </div>
 
                 <div className="w-full h-0 outline-[0.50px] outline-offset-[-0.25px] outline-zinc-300 my-7.5"></div>
 
-                {bgColor !== "" ? (
+                {resultTheme.bgColor !== "" ? (
                     <div className="w-full">
 
                         <div>
@@ -106,7 +150,7 @@ const AnalysisResultPage = () => {
                                 추정 사기 유형
                             </div>
                             <div className="w-full h-11 my-2.5 px-4 bg-blue-500 rounded-[10px] inline-flex justify-center items-center gap-1.5">
-                                <div className="text-white text-2xl font-bold font-['Pretendard'] leading-9">
+                                <div className="text-white text-2xl font-bold leading-9">
                                     {dummyResponse.data.estimatedFraudType}
                                 </div>
                             </div>
@@ -147,36 +191,30 @@ const AnalysisResultPage = () => {
 
 
                 <div className="mt-7.5 text-center text-black text-xl font-bold leading-loose">
-                    의심되면 <span className="text-blue-500">경찰청(112)</span> 
-                    <br/>
+                    의심되면 <span className="text-blue-500">경찰청(112)</span>
+                    <br />
                     또는 <span className="text-blue-500">금융감독원(1132)</span>
-                    <br/>
+                    <br />
                     로 확인 전화를 추천드려요.
                 </div>
             </div>
             {/* 여기까지 사기 분석 결과 내용*/}
-            <div className="w-full h-96 px-6 py-7 bg-gradient-highlight-2 rounded-tl-[20px] rounded-tr-[20px] inline-flex flex-col justify-start items-center gap-7">
+            <div className="w-full h-96 px-6 py-7.5 bg-gradient-highlight-2 rounded-tl-[20px] rounded-tr-[20px] inline-flex flex-col justify-start items-center gap-7">
                 <div className="text-center justify-center text-white text-2xl font-bold leading-9">
                     지금 이렇게 하세요
                 </div>
-                
-                <div className="w-full flex gap-2">
-                    <ReactingCard icon={<img className="w-6 h-7" src={PhoneCall} alt="전화 아이콘"/>} title={<>신고<br/>접수하기</>} btnText={"전화 연결"} handleBtn={function (): void {
-                        throw new Error("Function not implemented.");
-                    } }/>
-                    <ReactingCard icon={<img className="w-6 h-7" src={Siren} alt="사이렌 아이콘"/>} title={<>피해대응<br/>절차보기</>} btnText={"바로가기"} handleBtn={function (): void {
-                        throw new Error("Function not implemented.");
-                    } }/>
-                    <ReactingCard icon={<img className="w-6 h-7" src={SmartPhone} alt="스마트폰 아이콘"/>} title={<>보호자에게<br/>알리기</>} btnText={"공유하기"} handleBtn={function (): void {
-                        throw new Error("Function not implemented.");
-                    } }/>
+
+                <div className="w-full flex justify-between gap-2">
+                    <ReactingCard icon={<img className="w-6 h-7" src={PhoneCall} alt="전화 아이콘" />} title={<>신고<br />접수하기</>} btnText={"전화 연결"} handleBtn={handleCallClick} />
+                    <ReactingCard icon={<img className="w-6 h-7" src={Siren} alt="사이렌 아이콘" />} title={<>피해대응<br />절차보기</>} btnText={"바로가기"} handleBtn={handleReportClick} />
+                    <ReactingCard icon={<img className="w-6 h-7" src={SmartPhone} alt="스마트폰 아이콘" />} title={<>보호자에게<br />알리기</>} btnText={"공유하기"} handleBtn={handleGuardianClick} />
                 </div>
 
-                <div className="text-center justify-center text-white text-base font-bold underline leading-normal">
-                    홈으로 돌아가기 
-                </div>
+                <button className="text-center justify-center text-white text-base font-bold underline leading-normal" onClick={handleHomeClick}>
+                    홈으로 돌아가기
+                </button>
             </div>
-    
+
         </div>
     )
 }
--- src/pages/FraudSurvey/AnalysisResultPage/components/ReactingCard.tsx
+++ src/pages/FraudSurvey/AnalysisResultPage/components/ReactingCard.tsx
@@ -8,19 +8,16 @@ interface ReactingCardProps {
 const ReactingCard = ({ icon, title, btnText, handleBtn }: ReactingCardProps) => {
 
     return (
-        <div className="flex flex-col justify-between items-center h-44 bg-white/50 rounded-xl border-[0.81px] border-white/60 px-2.5 pt-5.5 pb-2">
-
-            <div>
-                <div className="w-11 h-11 bg-[radial-gradient(ellipse_50.00%_50.00%_at_50.00%_50.00%,_rgba(255,_255,_255,_0.73)_0%,_rgba(255,_255,_255,_0)_100%)] rounded-full">
-                    {icon}
-                </div>
+        <div className="flex flex-col justify-between items-center w-full h-44 bg-white/50 rounded-xl border-[0.81px] border-white/60 px-2.5 pt-5.5 pb-2">
+            <div className="inline-flex justify-center items-center w-11 h-11 bg-[radial-gradient(ellipse_50.00%_50.00%_at_50.00%_50.00%,_rgba(255,_255,_255,_0.73)_0%,_rgba(255,_255,_255,_0)_100%)] rounded-full">
+                {icon}
             </div>
 
             <div className="text-center text-slate-950 text-base font-bold leading-tight">
                 {title}
             </div>
 
-            <button className="h-8 px-5 py-1.5 bg-blue-500 rounded-lg inline-flex justify-center items-center gap-2.5"
+            <button className="w-full h-8 py-1.5 bg-blue-500 rounded-lg inline-flex justify-center items-center gap-2.5"
                 onClick={handleBtn}>
                 <div className="text-center text-white text-sm font-medium leading-tight">
                     {btnText}
