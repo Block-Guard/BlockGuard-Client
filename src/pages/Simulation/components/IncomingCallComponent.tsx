626d035dfc2a0f11cbc8fd2daf32b79d422196d2
holdn2 <yoocy01@gmail.com>
2025-07-27 11:31:40 +0900

    feat: 클릭 애니메이션 Lottie 추가

--- src/pages/Simulation/components/IncomingCallComponent.tsx
+++ src/pages/Simulation/components/IncomingCallComponent.tsx
@@ -1,3 +1,5 @@
+import Lottie from "lottie-react";
+import ClickAnimation from "../../../assets/lottie/click.json";
 import PhoneButton from "./PhoneButton";
 import SendMessageIcon from "@/assets/simulation/phone-button-icons/send-message-icon.svg";
 import VoicemailIcon from "@/assets/simulation/phone-button-icons/voicemail-icon.svg";
@@ -15,13 +17,19 @@ const IncomingCallComponent = ({ onClickToRespond }: Props) => {
         <PhoneButton icon={SendMessageIcon} desc="메시지 보내기" size="sm" />
         <PhoneButton icon={EndCallIcon} desc="거절" color="#f24e4e" />
       </div>
-      <div className="flex flex-col justify-between">
+      <div className="relative overflow-hidden flex flex-col justify-between">
         <PhoneButton icon={VoicemailIcon} desc="음성 사서함" size="sm" />
         <PhoneButton
           icon={RespondCallIcon}
           desc="응답"
           color="#66CF68"
           onClick={onClickToRespond}
+        />
+        <Lottie
+          animationData={ClickAnimation}
+          loop
+          autoplay
+          className="absolute bottom-17 right-11 translate-[50%] w-50 pointer-events-none"
         />
       </div>
     </div>
