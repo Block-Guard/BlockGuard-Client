d66e8f98508b8c46f2894c7ae2f2a1cdc75e01b1
holdn2 <yoocy01@gmail.com>
2025-07-31 15:10:18 +0900

    fix: 배포 시 오류 해결

--- src/pages/Simulation/PhishingSimulations/LoanInvestment/FakeLoanInvestPage/components/LoanInfoInput.tsx
+++ src/pages/Simulation/PhishingSimulations/LoanInvestment/FakeLoanInvestPage/components/LoanInfoInput.tsx
@@ -1,47 +1,63 @@
 import { useRef, useState } from "react";
 
 interface LoanInfoInputProps {
-    label: string;
-    placeholder: string;
-    inputState: boolean;
-    inputStateSetter: React.Dispatch<React.SetStateAction<boolean>>
-    isReminded: boolean;
+  label: string;
+  placeholder: string;
+  inputState: boolean;
+  inputStateSetter: React.Dispatch<React.SetStateAction<boolean>>;
+  isReminded: boolean;
 }
 
-export const LoanInfoInput = ({ label, placeholder, inputState, inputStateSetter, isReminded }: LoanInfoInputProps) => {
-    const textForm = useRef<HTMLInputElement>(null);
-    const [info, setInfo] = useState("");
+export const LoanInfoInput = ({
+  label,
+  placeholder,
+  inputState,
+  inputStateSetter,
+  isReminded,
+}: LoanInfoInputProps) => {
+  const textForm = useRef<HTMLInputElement>(null);
+  const [info, setInfo] = useState("");
 
-    const handleChange = (e) => {
-        setInfo(e.target.value);
-        inputStateSetter(true)
-    }
-    return (
-        <div className="flex flex-col">
-            {
-                (info === "") && isReminded ? (
-                    <span className="text-red-600 text-base font-medium leading-normal">
-                        {label}  &nbsp;{"*필수입력 항목입니다."}
-                    </span>
-                ) : (
-                    <span className="text-black text-base font-medium leading-normal">
-                        {label}
-                    </span>
-                )
-            }
+  console.log(inputState); // 배포 에러 해결
 
-            <input type="text"
-                className={`w-full h-12 px-5 py-3 outline-2 outline-offset-[-2px]
-                ${(info === "") && isReminded ? "outline-red-600":"outline-gray-100"}
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInfo(e.target.value);
+    inputStateSetter(true);
+  };
+  return (
+    <div className="flex flex-col">
+      {info === "" && isReminded ? (
+        <span className="text-red-600 text-base font-medium leading-normal">
+          {label} &nbsp;{"*필수입력 항목입니다."}
+        </span>
+      ) : (
+        <span className="text-black text-base font-medium leading-normal">
+          {label}
+        </span>
+      )}
+
+      <input
+        type="text"
+        className={`w-full h-12 px-5 py-3 outline-2 outline-offset-[-2px]
+                ${
+                  info === "" && isReminded
+                    ? "outline-red-600"
+                    : "outline-gray-100"
+                }
                             text-lg font-medium leading-relaxed text-slate-950
                                     focus:bg-monochrome-200
                                     placeholder-zinc-300
                                     resize-none no-scrollbar
-                                    ${info === "" ?  "bg-monochrome-200":"bg-white"}
+                                    ${
+                                      info === ""
+                                        ? "bg-monochrome-200"
+                                        : "bg-white"
+                                    }
                                     `}
-                placeholder={placeholder}
-                onChange={handleChange}
-                ref={textForm} />
-        </div>
-    )
-}
+        placeholder={placeholder}
+        onChange={handleChange}
+        ref={textForm}
+      />
+    </div>
+  );
+};
