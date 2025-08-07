ff08b356b5c75f5a1a10a6fd5bbd50532e0ebc25
glyph8 <newdy0808@gmail.com>
2025-08-07 14:13:31 +0900

    fix: 토스트 메세지 디테일 추가

--- src/components/Toast/PlainToast.tsx
+++ src/components/Toast/PlainToast.tsx
@@ -10,10 +10,11 @@ export function PlainToast(props: ToasterProps) {
       toastOptions={{
         classNames: {
           toast: "bg-[#EDEEF0] px-3 py-4 rounded-[10px]",
-          title: "text-[#000B25] text-xl font-medium leading-loose",
+          title: "text-[#000B25] text-[20px] font-medium font-['Pretendard'] leading-[1.5] tracking-[-0.4px]",
         },
         style:{
            maxWidth: "100vw",
+            // maxWidth: "calc(100vw - 50px)",
         }
       }}
     />
