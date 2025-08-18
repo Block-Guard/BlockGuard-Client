d8c26e4dc324b3738976f26ffc7b6c6c665adfb0
glyph8 <newdy0808@gmail.com>
2025-08-18 18:47:13 +0900

    chore: react-query 설치

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Router.tsx";
-
+import {QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import { Toaster } from "./components/ui/sonner";
 
+const clientQuery = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <QueryClientProvider client={clientQuery}>
     <RouterProvider router={router} />
     <Toaster />
+    <ReactQueryDevtools initialIsOpen={false}/>
+    </QueryClientProvider>
   </StrictMode>
 );
