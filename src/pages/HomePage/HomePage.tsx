50f1c587630c4316298c8d23e90662efb7bce61b
holdn2 <yoocy01@gmail.com>
2025-08-21 17:45:50 +0900

    fix: 첫 방문 시 온보딩 화면 뜨도록 수정

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,23 @@
+import { useEffect } from "react";
 import HomeCheck from "./components/HomeCheck";
 import NewsLines from "./components/NewsLines";
 import ReportResponse from "./components/ReportResponse";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (localStorage.getItem("first-use") !== "false") {
+      navigate("/onboarding");
+    }
+  }, []);
   return (
     <div className="overflow-y-scroll no-scrollbar mb-10 flex flex-col gap-8">
+      <HomeCheck />
       <HomeCheck />
       <ReportResponse />
       <NewsLines />
     </div>
   );
 };
-
 export default HomePage;
